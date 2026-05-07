755abfeb314854f5fb19d399522350da0ea81dd6
Tushar Chugh <tusharchugh09022004@gmail.com>
2026-05-07 10:22:37 +0530

    Update frontend to use deployed backend URL

--- stock-simulator/client/vite.config.js
+++ stock-simulator/client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
     '/api': {
-      target: 'http://localhost:5000',
+      target: 'https://stock-stimulator-1.onrender.com',
       changeOrigin: true,
       ws: true
     }
